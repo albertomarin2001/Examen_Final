dd7359f9d127228e73f1a42068adba5ea0bf43a7
Alberto Marin Estremadoiro <a2019116197@estudiantes.upsa.edu.bo>
2020-12-12 12:10:48 -0400

    Visual Terminado

---
+++ operaciones/operaciones/Controllers/Operaciones.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace operaciones.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OperacionesController : ControllerBase
+    {
+        [HttpGet]
+        public string Add(int numero)
+        {
+            if (numero == 0)
+                return "Alberto";
+            else if (numero < 0)
+                return "Error";
+            else
+                return $"{"https://image.freepik.com/vector-gratis/numeros-cartel-imagen_1639-6453.jpg"}";
+
+        }
+
+    }
+}
